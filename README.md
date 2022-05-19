102aa7f069ec719270febb3464fc1affecd8cfca
Enrique Gómez <n3wtongomez@hotmail.com>
2022-05-19 10:32:30 -0500

    readme y main

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # GPS-Ultrasonico
+
+Programa que ayuda a personas invidentes a traves de un modulo gps recibe la informacion de este, y una interfaz de usuario, en manera de audio, se pueda hacr busqueda de puntos de interes, o locaciones especificas para proseguir en crear una lista de direcciones y poder seguir estas de manera autonoma y caminando, complementando la seguridad el invidente con sensores ultrasonicos para verificar que no haya objetos en colision con el usuario.
+
+# Autor
+
+[Enrique Gómez](https://github.com/N3wtonGomez)
+
+[Jorge García](https://github.com/Jorge02342)
+
+# Contribuidores
+
+# Version 1.1
+Esta es una version estable, por ahora solo permite interactuar con el programa buscando las locaciones por tipo de interes, ingresando las categorias descritas en archivo yaml.
+
+# Instalación
+El software esta diseñado para usarse en una raspberry (no importa el modelo), el sistema operativo puede ser raspbian lite, o full. 
+
+Primero copia este repositorio en tu equipo, ya sea desde raspbian, o puedes enviarlo por [ssh](https://www.hostinger.mx/tutoriales/que-es-ssh). 
+#### 1. Copiar este repositorio
+```
+git clone https://github.com/N3wtonGomez/GPS-Ultrasonico
+```
+Puedes generar un [entorno vitual](https://www.freecodecamp.org/espanol/news/entornos-virtuales-de-python-explicados-con-ejemplos/), si deseas que las librerias usadas aquí, no interfieran con tu instalación de python de trabajo.
+#### 2. Instalamos las dependecias necesarias
+```
+pip install -r requirements.txt
+```
+
+# Hardware
+* [Raspberry Pi](https://www.amazon.com.mx/RASPBERRY-PI-MS-004-00000024-Raspberry-Model/dp/B01LPLPBS8/ref=sr_1_1?keywords=raspberry+pi+3+model+b&qid=1652974163&sprefix=raspberry%2Caps%2C761&sr=8-1&ufe=app_do%3Aamzn1.fos.8a46d436-f8dd-421d-a49c-494b5d1632c6).
+* [Sensor Ultrasonico](https://www.amazon.com.mx/IIVVERR-ultras%C3%B3nico-distancia-Ultrasonic-Measuring/dp/B07Y7VSP1K/ref=sr_1_8?keywords=sensor+ultrasonico+hc&qid=1652974210&sprefix=sensor+ultras%2Caps%2C179&sr=8-8), puedes poner hasta 3 sensores.
+* [Modulo gps](https://www.amazon.com.mx/Microcontrolador-Compatible-sensibilidad-posicionamiento-navegaci%C3%B3n/dp/B07P8YMVNT/ref=sr_1_1?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=IV3HI6P367D6&keywords=modulo+gps&qid=1652974265&sprefix=modulo+%2Caps%2C133&sr=8-1).
+
+# Como contribuir
+1. Crea un fork del repositorio en tu cuenta.
+2. Haz una rama de trabajo.
+```
+git branch <feature/nombre-de-tu-feature>
+```
+3. Escribe tu código.
+4. Agrega tu nombre a la secciond e contribuidores de este documento.
+5. Sube el código a tu repositorio.
+```
+git push -u origin <feature/nombre-de-tu-feature>
+```
+6. Crea tu [pull-request](https://www.freecodecamp.org/espanol/news/como-hacer-tu-primer-pull-request-en-github/).
--- main.py
+++ main.py
@@ -1,81 +1,134 @@
-import speech_recognition as sr
+# programa que ayuda a personas invidentes a traves de un modulo gps 
+# #recibe la informacion de este, y una interfaz
+# de usuario, en manera de audio, se pueda hacr busqueda de puntos de interes, o locaciones
+# especificas para proseguir en crear una lista de direcciones y poder seguir estas de manera
+# autonoma y caminando, complementando la seguridad el invidente con sensores ultrasonicos
+# para verificar que no haya objetos en colision con el usuario
+#
+# @utor: Enrique Gómez
+# @utor: Jorge Garcia
+# 
+# derechos reservados para Tomorrowlab inc.
+
+
+# libreria que realiza la busqueda de los puntos de interes
+# ademas de generar la lista de indicaciones a traves de un geojson
 import openrouteservice as ors
+# libreria que controla los pines gpio de la raspberry
+# donde estan conectados los sensores ultrasonicos, 
+# adaptados a entradas usb, para facilidad de conexion
+import RPi.GPIO as GPIO
+# se usa para poder hacer dos o mas procesos al mismo tiempo
+# de manera asincrona
+import threading as th
+# nos permite traducir los strings en voz 
 import pyttsx3
+# control del tiempo
+import time
+# manipulacion del archivo yml donde se encuentran almacenados
+# los codigos de clasificacion de los puntos de interes
 import yaml
 
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
+engine = pyttsx3.init() # inciamos el motor de voz
+# obtenemos la lista de las voces disponibles
+voices = engine.getProperty('voices') 
+# seteamos la voz a una voz femenina, aunque esta
+# se encuentra en ingles
 engine.setProperty('voice', voices[0].id)
 
-
-
+# para poder hacr uso de la libreria openrouteservice se necesita
+# una llave de seguridad de la api, la cual se puede obtener
+# inciando sesion en la pagina web, y creando una llave de manera
+# gratuita
+# https://openrouteservice.org/
 api_key = "5b3ce3597851110001cf62485f5107de0cc54c1db731a1aa335d7635" # api key necesaria para poder trabajar
-client =  ors.Client(key=api_key)
+client =  ors.Client(key=api_key) # hacemos la comunicacion con el sistema a traves de la api
 
+# generamos una variable json con la informacion primaria del usuario,
+# el tipo de ubicacion que vamos a usar, en este caso  "point", referente
+# a que solo usaremos un par de coordenadas
 geojson = {
     "type":"point",
-    "coordinates":[-102.262161, 21.879035] # coordenadas cerca de donde queremos buscar
+    "coordinates":[-102.262161, 21.879035] # coordenadas que cambiaran conforme el gps
 }
-coordinates = [-102.262161, 21.879035]# coordenadas de nuestra posicion
+# coordenadas variables de nuestra posicion gps
+coordinates = [-102.262161, 21.879035]
 
 def Talk(message):
-    engine.say(message)
-    engine.runAndWait()
-
-def Listener():
-    listener= sr.Recognizer()
-    try:
-        with sr.Microphone() as source:
-            print("Hola, ¿a donde quieres ir hoy?")
-            listener.adjust_for_ambient_noise(source)
-            audio = listener.listen(source)
-            text = listener.recognize_bing(audio, lenguage="es")
-            return text.lower()
-    except:
-        Talk("No te eh entendido")
+    # como parte de la interfaz de usuario se, usa la funcion pra que el motor de voz
+    # pueda decir la informacion de salida de nuestro software, este necesita recibir
+    # un string
+    engine.say(message) # al motor le mandamos el string 
+    engine.runAndWait() # y esperamos a que termine de hablar
 
 def pois(id):
+    # esta funcion nos permite encontrar puntos de interes, cerca de nuestra ubicacion
+    # necesita que le mandemos el id, de lo que deseamos buscar
     pois = client.places(
-        request='pois',
-        geojson=geojson, # archivo o variable con la informacion
+        request='pois', # peticion de puntos de interes
+        # archivo o variable con la informacion geografica del usuario
+        geojson=geojson, 
         buffer=2000, # rango de metros en diametro del area de busqueda
-        # hospital: 206, restaurant: 570
-        filter_category_ids=[id]
+        filter_category_ids=[id] # categoria de la bsuqueda, estas se encuentran
+        # en el archivo yml
     )
+    # se retorna un archivo yml, con todos los puntos de interes
+    # con la informacion completas de estos, nombre, y coordenadas son las
+    # que nos interesan
     return pois
 
 def getCoordinates(feature):
+    # dado el arhcivo yml, por cada punto de interes, con la seccion de "features"
+    # vamos a devolver las coordenadas de las locaciones de interes
     geometry = feature["geometry"]
     return geometry["coordinates"]
 
 def getName(feature):
+    # dado el arhcivo yml, por cada punto de interes, con la seccion de "features"
+    # vamos a devolver el nombre de las locaciones de interes
     properties = feature["properties"]
     osm = properties["osm_tags"]
     return osm["name"]
 
 def getRuta(coor1, coor2):
+    # el usario ya habiendo elegido la locacion a la que quiere llegar
+    # hay que obtener los datos para llegar a ella,
+    # recibimos dos pares de coordenadas, el punto de origen (el usuario),
+    # y el destino (la locacion de interes)
     coordenadas = [coor1, coor2]
     # obtenemos toda las geo instrucciones, con la funcion 'directions'a la que le vamos a dar de parametros,
     # el como nos vamos a mover, y el como queremos obtener la informacion
     ruta = client.directions(coordinates=coordenadas, profile='foot-walking', format='geojson')
-    return ruta
+    return ruta # devolvemos un archivo yml con las instrucciones especificas
 
 def getPasos(info):
-    nombre = info["instruction"]
-    return nombre
+    # el archivo yml que recibe el usario estamos en la seccion de pasos, y 
+    # vamos a devolver al usario por cada paso, la instruccion necesaria
+    return info["instruction"]
 
 def getInstrucciones(features):
-    secciones = features[0]
-    propertys = secciones["properties"]
-    segmentos = propertys["segments"]
-    pasos = segmentos[0]
+    # con el archivo yml vamos a leer la informacion de nuestro viaje
+    secciones = features[0] # la primer seccion es la que nos da las instrucciones
+    # hay que ir desglosando para poder llegar al texto con las instrucciones 
+    # especificas
+    propertys = secciones["properties"] # sacamos las propiedades
+    segmentos = propertys["segments"] # los segmentos
+    # el primer segmento contiene la informacion de la duracion del viaje
+    # la distancia caminada, y un diccionario con las instrucciones 
+    pasos = segmentos[0] 
+    # convertimos los segundos de trayecto que nos da, a minutos
     minutos = int(pasos["duration"])/60
+    # convertimos los metros que nos da a km
     km =  int(pasos["distance"])/1000
+    # impirmimos en pantalla y hablamos la duracion del trayecto
     print("su viaje durará ", round(minutos) , " minutos")
     Talk(f"su viaje durará {round(minutos)} minutos")
+    # impirmimos en pantalla y hablamos la distancia del trayecto
     print("su viaje será de ", km, " kilometros")
     Talk(f"su viaje será de {km} kilometros")
+    # buscamos los pasos exactos que necesita nuestro viaje
     steps = pasos["steps"]
+    # para cada instruccion sacamos los pasos y los leemos
     for i in range(0, len(steps)):
         print(getPasos(steps[i]))
         Talk(getPasos(steps[i]))
